5b55dac17a2a18dcd90282c529dd4df36b038fb9
alexmbeasley <alexmbeasley@gmail.com>
2023-11-16 15:07:12 -0600

    wiki page loading, some thumbnails loading

--- client/components/BirdProfile.jsx
+++ client/components/BirdProfile.jsx
@@ -2,32 +2,92 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const BirdProfile = ({ bird, userId, birdSightings }) => {
-  const [wikiUrl, setWikiUrl] = useState(null);
+  const [wikiDetails, setWikiDetails] = useState({
+    scientificUrl: null,
+    commonThumbnailUrl: null,
+  });
 
   useEffect(() => {
     const fetchBirdDetails = async () => {
       try {
-        const searchTerm = encodeURIComponent(
+        const scientificSearchTerm = encodeURIComponent(
+          bird.scientificName.replace(/[^a-zA-Z0-9 ]/g, "")
+        );
+
+        const commonSearchTerm = encodeURIComponent(
           bird.commonName.replace(/[^a-zA-Z0-9 ]/g, "")
         );
-        const wikiApiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&inprop=url&titles=${searchTerm}`;
-        const wikiResponse = await axios.get(wikiApiUrl);
 
-        const pages = wikiResponse.data.query.pages;
-        const firstPageId = Object.keys(pages)[0];
-        const wikiUrl = pages[firstPageId].fullurl;
+        // Fetch Wikipedia details for scientific name including URL
+        const scientificWikiApiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=info&inprop=url&titles=${scientificSearchTerm}`;
+        const scientificWikiResponse = await axios.get(scientificWikiApiUrl);
 
-        setWikiUrl(wikiUrl);
+        const scientificPages = scientificWikiResponse.data.query.pages;
+        const scientificFirstPageId = Object.keys(scientificPages)[0];
+        const scientificUrl =
+          scientificPages[scientificFirstPageId]?.fullurl || null;
+
+        // Fetch Wikipedia details for common name including thumbnail
+        const commonWikiApiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=thumbnail&pithumbsize=200&titles=${commonSearchTerm}`;
+        const commonWikiResponse = await axios.get(commonWikiApiUrl);
+
+        const commonPages = commonWikiResponse.data.query.pages;
+        const commonFirstPageId = Object.keys(commonPages)[0];
+        const commonThumbnailUrl =
+          commonPages[commonFirstPageId]?.thumbnail?.source || null;
+
+        setWikiDetails({
+          scientificUrl: scientificUrl,
+          commonThumbnailUrl: commonThumbnailUrl,
+        });
       } catch (error) {
         console.error("Error fetching bird details:", error.message);
+        setWikiDetails({
+          scientificUrl: null,
+          commonThumbnailUrl: null,
+        });
       }
     };
 
     fetchBirdDetails();
-  }, [bird.commonName]);
+  }, [bird.scientificName, bird.commonName]);
 
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{
+        border: "2px solid #333",
+        backgroundColor: "#ddd",
+        padding: "10px",
+        margin: "10px",
+      }}
+    >
+      {wikiDetails.scientificUrl && (
+        <div className="card-content">
+          <p>Learn more about the {bird.commonName} on Wikipedia:</p>
+          <a
+            href={wikiDetails.scientificUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#0066cc", textDecoration: "none" }}
+          >
+            {wikiDetails.scientificUrl}
+          </a>
+        </div>
+      )}
+      {wikiDetails.commonThumbnailUrl && (
+        <div className="card-content">
+          <img
+            src={wikiDetails.commonThumbnailUrl}
+            alt={bird.commonName}
+            style={{
+              width: "150px",
+              height: "150px",
+              border: "1px solid #666",
+            }}
+          />
+        </div>
+      )}
       <div className="card-content">
         <p className="title">{bird.commonName}</p>
         <p className="subtitle">{bird.scientificName}</p>
@@ -35,13 +95,6 @@ const BirdProfile = ({ bird, userId, birdSightings }) => {
         <p>Total Observed: {bird.totalObserved}</p>
         <p>Observation Date: {bird.observationDate}</p>
       </div>
-      {wikiUrl && (
-        <div className="card-footer">
-          <a href={wikiUrl} target="_blank" rel="noopener noreferrer">
-            Learn more on Wikipedia
-          </a>
-        </div>
-      )}
     </div>
   );
 };
