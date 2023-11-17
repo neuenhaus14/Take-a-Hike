5e47a8074750a73859ddf6b43f19f53c52691e6d
rvierbuc <ranlikerats@gmail.com>
2023-11-16 19:45:18 -0600

    troubleshooting deployment solo

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
-const srcDir = path.resolve(__dirname, "client");
-const distDir = path.resolve(__dirname, "dist");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const srcDir = path.resolve(__dirname, 'client');
+const distDir = path.resolve(__dirname, 'dist');
 
 
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   watch: true,
   watchOptions: {
-    aggregateTimeout: 200,
+    ignored: /node_modules/,
+    aggregateTimeout: 300,
     poll: 1000,
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   stats: {
     excludeModules: /node_modules/,
   },
   entry: {
-    app: path.resolve(srcDir, "index.jsx"),
+    app: path.resolve(srcDir, 'index.jsx'),
   },
   output: {
     path: distDir,
-    filename: "[name].bundle.js",
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
@@ -30,31 +31,33 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(srcDir, "index.html"),
-      inject: "body",
+      template: path.resolve(srcDir, 'index.html'),
+      inject: 'body',
     }),
     new NodePolyfillPlugin()
   ],
   resolve: {
     fallback: {
-        "fs": false
+      'fs': false
     },
-}
-  // target: 'node',
+    extensions: ['.js', '.jsx'],
+    
+  },
+  target: 'node',
 };
 
 
