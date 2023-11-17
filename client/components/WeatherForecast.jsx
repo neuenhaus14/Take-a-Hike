38f14dfb1baa4852e5f0016a5185b803dd49792d
SNelson723 <stephenn649@gmail.com>
2023-11-17 15:39:13 -0600

    Fixed the dates rendered in weather component, data was correct but the new Date instances were returning the previous date instead of the current date

--- client/components/WeatherForecast.jsx
+++ client/components/WeatherForecast.jsx
@@ -12,9 +12,9 @@ const WeatherForecast = ({ forecast }) => {
       </div>
       <div className="weather-conditions">
         <div className="info-container-top">
-          <span className="weather-info">Temp: {forecast.day.avgtemp_f}</span>
-          <span className="weather-info"> High: {forecast.day.maxtemp_f} </span>
-          <span className="weather-info"> Low: {forecast.day.mintemp_f}</span>
+          <span className="weather-info">Temp: {forecast.day.avgtemp_f} °F</span>
+          <span className="weather-info"> High: {forecast.day.maxtemp_f} °F</span>
+          <span className="weather-info"> Low: {forecast.day.mintemp_f} °F</span>
         </div>
         <div className="info-container-bottom">
           <span className="weather-info">Chance of Rain: {forecast.day.daily_chance_of_rain}% </span>
